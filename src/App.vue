8da91d88ca8ead4a98db1403dcbeb992d7650517
bboampong <baffouraduboampong@gmail.com>
2019-07-01 17:51:58 +0000

    Completed Website layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
-  <div id="app" class="w-3/5 mx-auto p-6">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to WeFlyp"/>
-    <div class="bg-purple-600 text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange">
-      Test
-    </div>
+  <div id="app">
+    <Hero/>
+    <HowItWorks/>
+    <Features/>
+    <Suppliers/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/Hero'
+import HowItWorks from './components/HowItWorks'
+import Features from './components/Features'
+import Suppliers from './components/Suppliers'
+import Footer from './components/Footer'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Hero,
+    HowItWorks,
+    Features,
+    Suppliers,
+    Footer
   }
 }
 </script>
