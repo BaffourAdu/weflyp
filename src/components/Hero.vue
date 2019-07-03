5c41a9675cfc4f22e9c965c3a61bee87b6ae1959
bboampong <baffouraduboampong@gmail.com>
2019-07-03 17:06:48 +0000

    Completed How it works section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,11 @@
 <template>
-    <header class="h-full">
+    <header class="h-full mb-4">
         <div class="bg-cover bg-center bg-auto top-header relative" style="height: 40vh;">
            <img src="../assets/images/torn.svg" alt="Torn Image" class="absolute inset-x-0 bottom-0">
         </div>
         <div class="bg-white sm:py-20 py-8 xl:px-64 md:px-8 px-6 bottom-header text-center" style="height: 60vh;">
             <h1 class="lg:text-5xl text-3xl leading-tight text-gray-800 tracking-wider font-semibold">Start Your Business from Home with Zero Capital </h1>
-            <p class="md:text-xl text-lg py-4 leading-snug mb-4 tracking-wide text-gray-700">WeFlyp provides you with all the Tools & Support you need to start and run a business <br class="lg:block hidden">
+            <p class="md:text-xl text-lg py-4 leading-snug mb-4 tracking-wide text-gray-700">One platform with all the ecommerce, tools and features you need to start, run, and grow your business  <br class="lg:block hidden">
                  without the hassle of managing Inventory, Suppliers, Packaging, or Delivery.</p>
             <a href="#" title="Get Early Access" class="hover:bg-white text-white bg-purple-700 hover:text-purple-700 no-underline tracking-wide uppercase rounded-lg text-sm font-bold shadow-md hover:shadow py-4 px-6">
                 Get Early Access
--- src/components/HowItWorks.vue
+++ src/components/HowItWorks.vue
@@ -1,31 +1,37 @@
 <template>
-    <div class="h-auto py-12 xl:px-48 md:px-12 px-6">
-        <div class="bg-white w-full h-32 p-6 border-2 border-black">
-            <h1>How does WeFlyp work?</h1>
-            <p>
+    <div class="bg-white h-auto py-12 xl:px-64 md:px-8 px-6 mb-4">
+        <div class="bg-white w-full h-54 p-6">
+            <h1 class="lg:text-5xl text-3xl leading-tight text-gray-800 tracking-wider font-semibold">How does WeFlyp work?</h1>
+            <p class="md:text-xl text-lg py-4 leading-snug tracking-wide text-gray-700">
                 Reselling with WeFlyp is as easy as sharing pictures 
-                with your customers. No more up front inventory costs or shipping logistics.  
+                with your customers. Earn high profit margins & weekly bonus. No more up front inventory costs or shipping logistics.  
             </p>
         </div>
         <div class="w-full flex flex-col md:flex-shrink lg:flex-row mt-4 text-center">
             <div class="md:flex w-full m-0  object-center">
-                <div class="bg-white md:w-2/4 h-64 mt-2 p-6 border-2 border-black">
-                    <img src="../assets/images/browse.jpg" alt="Browse Icon" class="object-none object-center">
-                    <h1>Browse Products</h1>
+                <div class="flex flex-col justify-center items-center bg-white md:w-2/4 h-64 mt-4 lg:ml-2 p-6">
+                    <img class="h-32" src="../assets/images/browse.jpg" alt="Browse Icon">
+                    <h1 class="text-xl leading-tight text-gray-800 tracking-wider font-semibold mt-2">1. Browse Products</h1>
+                    <p class="md:text-xl text-lg leading-snug tracking-wide text-gray-700 mt-2 mb-4">Choose the products you want to 
+                        sell from the best suppliers globally</p>
                 </div>
-                <div class="bg-white md:w-2/4 h-64 md:ml-2 mt-2 p-6 border-2 border-black">
-                    <img src="../assets/images/share.png" alt="">
-                    <h1>Share with Customers</h1>
+                <div class="flex flex-col justify-center items-center bg-white md:w-2/4 h-64 md:ml-2 mt-4 p-6">
+                    <img class="h-24 mb-2 mt-4" src="../assets/images/share.png" alt="Share Icon">
+                    <h1 class="text-xl leading-tight text-gray-800 tracking-wider font-semibold mt-4">2. Share with Customers</h1>
+                    <p class="md:text-xl text-lg leading-snug tracking-wide text-gray-700 mt-2 mb-4">Add your margins and Sell products to anyone, 
+                    anywhere</p>
                 </div>
             </div>
             <div class="md:flex w-full m-0">
-                <div class="bg-white md:w-2/4 h-64 mt-2 lg:ml-2 p-6 border-2 border-black">
-                    <img src="../assets/images/order.jpg" alt="">
-                    <h1>place Order</h1>
+                <div class="flex flex-col justify-center items-center bg-white md:w-2/4 h-64 mt-4 lg:ml-2 p-6">
+                    <img class="h-32" src="../assets/images/order.jpg" alt="Place Order Icon">
+                    <h1 class="text-xl leading-tight text-gray-800 tracking-wider font-semibold mt-2">3. Place Order</h1>
+                    <p class="md:text-xl text-lg leading-snug tracking-wide text-gray-700 mt-2 mb-4">Place an order on WeFlyp on behalf of your customer</p>
                 </div> 
-                <div class="bg-white md:w-2/4 h-64 md:ml-2 mt-2 p-6 border-2 border-black">
-                    <img src="../assets/images/deliver.jpg" alt="">
-                    <h1>Order Delivered</h1>
+                <div class="flex flex-col justify-center items-center bg-white md:w-2/4 h-64 md:ml-2 mt-4 p-6">
+                    <img class="h-32" src="../assets/images/deliver.jpg" alt="Delivery Icon">
+                    <h1 class="text-xl leading-tight text-gray-800 tracking-wider font-semibold mt-2">4. Order Delivered</h1>
+                    <p class="md:text-xl text-lg leading-snug tracking-wide text-gray-700 mt-2 mb-4">Our Supplier ships the order directly to your customer</p>
                 </div>
             </div>
         </div>
