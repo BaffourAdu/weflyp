fd4578ead310803d81f9892cfa55f833779cd734
bboampong <baffouraduboampong@gmail.com>
2019-07-04 18:35:39 +0000

    Fixed Features Section Rsponsiveness

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="h-auto py-24 xl:px-64 sm:px-4 px-8" style="background-color: #F5F6FB">
-      <div class="lg:w-2/3 mx-auto">
+      <div class="md:w-11/12 mx-auto">
         <div class="flex sm:flex-row flex-col-reverse">
-          <div class=" md:w-3/4 h-64 mt-2 py-12">
+          <div class="md:w-3/4 h-64 mt-2 py-12 sm:px-12 lg:pr-48">
             <h1 class="text-3xl leading-tight text-gray-800 tracking-wider font-semibold mt-2">Start your business journey</h1>
             <p class="md:text-xl text-lg leading-snug tracking-wide text-gray-700 mt-2 mb-2 mt-4">
               Anyone can Resell with WeFlyp - students, housewives, boutique owners, teachers, professionals etc.
@@ -15,10 +15,10 @@
           </div>
         </div>
         <div class="flex sm:flex-row flex-col mt-24 mb-24">
-          <div class="md:w-2/4 h-64 mt-2 mb-10 py-12">
+          <div class="md:w-2/4 h-64 mt-2 mb-10 py-12 lg:px-10">
             <img class="h-64 float-left" src="../assets/images/social.jpg" alt="Delivery Icon">
           </div>
-          <div class="md:w-2/4 h-64 mt-2 md:ml-2">
+          <div class="md:w-3/5 h-64 mt-2 py-12 sm:px-12">
             <h1 class="text-3xl leading-tight text-gray-800 tracking-wider font-semibold mt-2">Turn family, friend & followers into customers</h1>
             <p class="md:text-xl text-lg leading-snug tracking-wide text-gray-700 mt-2 mb-2 mt-4">
               Pick the products you can sell most and share pictures these products with your Family, Friends and followers either through 
