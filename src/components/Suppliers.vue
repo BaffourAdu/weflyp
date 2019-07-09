7bb78dc6ba114141a0a110fc16ec58e52c002270
bboampong <baffouraduboampong@gmail.com>
2019-07-09 20:15:44 +0000

    Updated Suppliers

--- src/components/Suppliers.vue
+++ src/components/Suppliers.vue
@@ -2,10 +2,29 @@
     <section class="bg-cover bg-center bg-auto w-full h-auto py-12 xl:px-64 md:px-8 px-4 mt-64 suppliers">
       <div class="flex flex-col bg-white -mt-64 rounded-lg shadow-md h-auto lg:w-3/6 md:2/3 w-full md:p-12 p-6">
         <h1 class="text-3xl leading-tight text-gray-800 tracking-wider font-semibold mt-2">Sell your products through the largest distribution network </h1>
-        <p class="md:text-lg text-base leading-normal tracking-wider text-gray-700 mt-2 mb-2 mt-6">
-          Our team helps you list your products on WeFlyp. As your products go live on the app and our resellers share them with their WhatsApp, 
-          Facebook and Instagram contacts, you will start receiving orders immediately. We give you priority space for maximum visibility as you become popular.
-        </p>
+        <ul class="mt-4">
+          <li class="mt-3 flex">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 mt-px mr-3 flex-shrink-0"><circle cx="12" cy="12" r="10" class="text-purple-200 fill-current"></circle><path d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z" class="text-purple-600 fill-current"></path></svg>
+            <span class="-mt-px">Our team helps you list your products on WeFlyp</span>
+          </li>
+          <li class="mt-3 flex">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 mt-px mr-3 flex-shrink-0"><circle cx="12" cy="12" r="10" class="text-purple-200 fill-current"></circle><path d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z" class="text-purple-600 fill-current"></path></svg>
+            <span class="-mt-px">Your products go live immediately on the app for our resellers share with their customers</span>
+          </li>
+          <li class="mt-3 flex">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 mt-px mr-3 flex-shrink-0"><circle cx="12" cy="12" r="10" class="text-purple-200 fill-current"></circle><path d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z" class="text-purple-600 fill-current"></path></svg>
+            <span class="-mt-px">We offer quick and convenient pick-up and delivery through our logistics partners</span>
+          </li>
+          <li class="mt-3 flex">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 mt-px mr-3 flex-shrink-0"><circle cx="12" cy="12" r="10" class="text-purple-200 fill-current"></circle><path d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z" class="text-purple-600 fill-current"></path></svg>
+            <span class="-mt-px">All you need to do is pack the product and we do the rest</span>
+          </li>
+          <li class="mt-3 flex">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 mt-px mr-3 flex-shrink-0"><circle cx="12" cy="12" r="10" class="text-purple-200 fill-current"></circle><path d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z" class="text-purple-600 fill-current"></path></svg>
+            <span class="-mt-px">We give you priority space for maximum visibility as you become popular</span>
+          </li>
+        </ul>
+
         <div class="mt-8 mb-6">
           <a href="https://forms.gle/36C1i3FsUbFbeP938" title="Become a Supplier" class="hover:bg-white text-white bg-purple-700 hover:text-purple-700 no-underline tracking-wide uppercase rounded-lg text-sm font-bold shadow-md hover:shadow py-4 px-6">
             Become a Supplier
